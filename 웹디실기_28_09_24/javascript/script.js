63da4f580b734191c4ad2fe1a36967dbe75f4b6e
elena7993 <elenalee7993@gmail.com>
2024-09-28 17:15:47 +0900

    28_09_24
    연습!

---
+++ 웹디실기_28_09_24/javascript/script.js
@@ -0,0 +1,11 @@
+jQuery(document).ready(function(){
+
+  $(`.menu`).mouseover(function(){
+    $(`.sub_menu_wrap`).stop().slideDown(500);
+    $(`#menu_bg`).stop().slideDown(500);
+  }).mouseout(function(){
+    $(`.sub_menu_wrap`).stop().slideUp(500);
+    $(`#mene_bg`).stop().slideUp(500);
+  });
+
+});
